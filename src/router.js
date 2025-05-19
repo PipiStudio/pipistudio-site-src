32b563f8877aa4a97fc850a43d266959f9ce237b
Aaron <aaronhustudent@163.com>
2025-05-19 22:14:31 +0800

    初始化更新，有了MVP

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Navigo from "navigo"
+
+const router = new Navigo('/');
+
+router.on("/", function () {
+  document.title = "呆呆游研社";
+  fetch("src/pages/home.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("app").innerHTML = data;
+    });
+});
+
+router.on("/projects", function () {
+  document.title = "项目";
+  fetch("src/pages/projects.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("app").innerHTML = data;
+    });
+});
+
+router.resolve()
